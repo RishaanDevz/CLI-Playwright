de26bad683e72301287ebcc12dcf963f98ac8346
RishaanDevz <113069412+RishaanDevz@users.noreply.github.com>
2024-05-31 20:25:26 -0400

    Update cli.js

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
 // cli.js
 const readline = require('readline');
 const { playSong } = require('./youtube');
-const { borrowBook } = require('./libbyborrowing');
+const { borrowBook } = require('./libby');
 
 const rl = readline.createInterface({
     input: process.stdin,
